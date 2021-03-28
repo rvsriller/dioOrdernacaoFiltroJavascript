ec812fad01d9dbb42b86b14f6efde17b717eae84
Riller <rvsriller@gmail.com>
2021-03-28 01:12:37 -0300

    Desafio 2 - Listas ordenadas de produtos de supermercado

---
+++ 2-ListasOrdenadas.js
@@ -0,0 +1,9 @@
+/*
+Esse algoritmo é responsável por ler um número x de listas na entrada, 
+essas listas possuem itens que devem ser comprados no supermercado, porém, alguns desses
+itens podem estar repetidos. Durante o processamento, esses itens repetidos serão removidos.
+Dessa maneiras, todas as listas estarão otimizadas após o processamento e os itens são ordenados
+em ordem alfabética.
+
+Há métodos utilizando funções como Push, Split e Sort para solucionar esse desafio.
+*/
